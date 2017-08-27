41a5f37cf98ad5b7fe336c81184ac9eaf1f321a8
Chris Franco <11533365+Francommit@users.noreply.github.com>
2017-08-27 09:47:53 +0100

    Moved choco installs to seperate files

---
+++ 3_main_install.ps1
@@ -0,0 +1,42 @@
+# TO-DO: Add default 'everything' shortcut of win+alt+spacebar
+# TO-DO: Add ahk scripts to Windows10 'startup' folder. Changed with the creators update.
+
+
+# Install Fundamentals
+choco install vcredist2015 -y 
+choco install jre8 -y 
+choco install 7zip -y 
+choco install googleChrome -y 
+choco install flashplayerplugin -y 
+choco install libreoffice -y
+choco install reddit-wallpaper-changer -y
+
+# Install Common Languages
+choco install python -y
+choco install nodejs.install -y
+choco install autohotkey -y
+
+# Install Source Management apps
+choco install git -y
+choco install kdiff3 -y
+choco install gitextensions -y
+choco install everything -y
+
+# Install IDEs
+choco install visualstudiocode -y
+choco install sublimetext3 -y
+choco install visualstudio2017-installer
+
+# Install Communication applications
+choco install skype -y
+choco install steam -y 
+
+# Install Img tools
+choco install imgburn -y 
+choco install rufus -y
+choco install gimp2 -y 
+
+# Install Network tools
+choco install putty -y
+choco install deluge -y
+choco install teamviewer -y
