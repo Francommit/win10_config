54533c3b4f922152e0fa64de44ed264abd6f8427
Chris Franco <11533365+Francommit@users.noreply.github.com>
2017-08-27 22:27:36 +1000

    Update 4_post_install.ps1

--- 4_post_install.ps1
+++ 4_post_install.ps1
@@ -1,2 +1,24 @@
 # TO-DO: Add default 'everything' shortcut of win+alt+spacebar
 # TO-DO: Add ahk scripts to Windows10 'startup' folder. Changed with the creators update.
+
+function Pin-App { param(
+[string]$appname,
+[switch]$unpin
+)
+try{
+if ($unpin.IsPresent){
+((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'From "Start" UnPin|Unpin from Start'} | %{$_.DoIt()}
+return "App '$appname' unpinned from Start"
+}else{
+((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'To "Start" Pin|Pin to Start'} | %{$_.DoIt()}
+return "App '$appname' pinned to Start"
+}
+}catch{
+Write-Error "Error Pinning/Unpinning App! (App-Name correct?)"
+}
+}
+
+
+Pin-App "Google Chrome" 
+Pin-App "Control Panel"
+Pin-App "This PC"
