858c29e4ccb27ce8f0a901491bdd6f72b6c628e0
Chris Franco <11533365+Francommit@users.noreply.github.com>
2017-08-27 11:56:47 +0100

    Updated gimp version

--- 3_main_install.ps1
+++ 3_main_install.ps1
@@ -1,4 +1,4 @@
-# TO-DO: Add default 'everything' shortcut of win+alt+spacebar
+ TO-DO: Add default 'everything' shortcut of win+alt+spacebar
 # TO-DO: Add ahk scripts to Windows10 'startup' folder. Changed with the creators update.
 
 
