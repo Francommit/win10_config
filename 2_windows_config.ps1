4a66c2b5dde7ce98dd178712fd4be176e318c0ec
Chris Franco <11533365+Francommit@users.noreply.github.com>
2017-08-27 10:40:58 +0100

    Update and rename 2_main_install.ps1 to 2_windows_config.ps1

---
+++ 2_windows_config.ps1
@@ -0,0 +1,98 @@
+Import-Module -DisableNameChecking $PSScriptRoot\..\lib\take-own.psm1
+Import-Module -DisableNameChecking $PSScriptRoot\..\lib\force-mkdir.psm1
+
+echo "Elevating privileges for this process"
+do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
+
+echo "Disabling Microsoft Telemetry Collection"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name AllowTelemetry -Type DWord -Value 0
+
+echo "Setting Power Settings: Disable Hibernation, Disable Standby, Set Various Timeouts"
+powercfg /h off
+powercfg -change -hibernate-timeout-dc 0
+powercfg -change -hibernate-timeout-ac 0
+powercfg -change -standby-timeout-dc 0
+powercfg -change -standby-timeout-ac 0
+powercfg -change -monitor-timeout-dc 0
+powercfg -change -monitor-timeout-ac 30
+powercfg -change -disk-timeout-dc 180
+powercfg -change -disk-timeout-ac 180
+
+echo "Setting Windows Explorer settings: Show Hidden Files, Show File Extension, Open Explorer default to This PC"
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoRestartShell -Value 0
+#Stop-Process -Force -ProcessName "explorer"
+$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty $key Hidden 1
+Set-ItemProperty $key HideFileExt 0
+Set-ItemProperty $key LaunchTo 1
+Set-ItemProperty $key ShowTaskViewButton 0
+
+# Show "This PC" on desktop
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -Value 0
+
+
+echo "Uninstalling default apps"
+$apps = @(
+    # default Windows 10 apps
+    "Microsoft.3DBuilder"
+    "Microsoft.Appconnector"
+    "Microsoft.BingFinance"
+    "Microsoft.BingNews"
+    "Microsoft.BingSports"
+    "Microsoft.BingWeather"    
+    "Microsoft.Getstarted"
+    "Microsoft.MicrosoftOfficeHub"
+    "Microsoft.MicrosoftSolitaireCollection"    
+    "Microsoft.People"
+    "Microsoft.SkypeApp"
+    "Microsoft.WindowsAlarms"
+    "Microsoft.WindowsCamera"
+    "Microsoft.WindowsMaps"
+    "Microsoft.WindowsPhone"    
+    "microsoft.windowscommunicationsapps"
+    "Microsoft.MinecraftUWP"    
+    "Microsoft.CommsPhone"
+    "Microsoft.ConnectivityStore"
+    "Microsoft.Office.Sway"    
+    "Microsoft.WindowsFeedbackHub"    
+    "Microsoft.BingFoodAndDrink"
+    "Microsoft.BingTravel"
+    "Microsoft.BingHealthAndFitness"
+    "Microsoft.WindowsReadingList"    
+    "9E2F88E3.Twitter"
+    "PandoraMediaInc.29680B314EFC2"
+    "Flipboard.Flipboard"
+    "ShazamEntertainmentLtd.Shazam"
+    "king.com.CandyCrushSaga"
+    "king.com.CandyCrushSodaSaga"
+    "king.com.*"
+    "ClearChannelRadioDigital.iHeartRadio"
+    "4DF9E0F8.Netflix"
+    "6Wunderkinder.Wunderlist"
+    "Drawboard.DrawboardPDF"
+    "2FE3CB00.PicsArt-PhotoStudio"
+    "D52A8D61.FarmVille2CountryEscape"
+    "TuneIn.TuneInRadio"
+    "GAMELOFTSA.Asphalt8Airborne"
+    "DB6EA5DB.CyberLinkMediaSuiteEssentials"
+    "Facebook.Facebook"
+    "flaregamesGmbH.RoyalRevolt2"
+    "Playtika.CaesarsSlotsFreeCasino"
+    "A278AB0D.MarchofEmpires"
+    "KeeperSecurityInc.Keeper"
+    "ThumbmunkeysLtd.PhototasticCollage"
+)
+
+foreach ($app in $apps) {
+    echo "Trying to remove $app"
+
+    Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage
+
+    Get-AppXProvisionedPackage -Online |
+        where DisplayName -EQ $app |
+        Remove-AppxProvisionedPackage -Online
+}
+
+# Prevents "Suggested Applications" returning
+force-mkdir "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Cloud Content"
+sp "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Cloud Content" "DisableWindowsConsumerFeatures" 1
