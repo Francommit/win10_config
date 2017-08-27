2f54dc05eec1a29e23b9fd4f0f410f9d6913b7ec
Chris Franco <11533365+Francommit@users.noreply.github.com>
2017-08-27 11:07:12 +0100

    Added unpinning logic

--- 2_windows_config.ps1
+++ 2_windows_config.ps1
@@ -86,3 +86,26 @@ foreach ($app in $apps) {
         where DisplayName -EQ $app |
         Remove-AppxProvisionedPackage -Online
 }
+
+
+function Pin-App {    param(
+        [string]$appname,
+        [switch]$unpin
+    )
+    try{
+        if ($unpin.IsPresent){
+            ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'Von "Start" lösen|Unpin from Start'} | %{$_.DoIt()}
+            return "App '$appname' unpinned from Start"
+        }else{
+            ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | ?{$_.Name -eq $appname}).Verbs() | ?{$_.Name.replace('&','') -match 'An "Start" anheften|Pin to Start'} | %{$_.DoIt()}
+            return "App '$appname' pinned to Start"
+        }
+    }catch{
+        Write-Error "Error Pinning/Unpinning App! (App-Name correct?)"
+    }
+}
+
+echo "Unpinning stock applications"
+Pin-App "xbox" -unpin
+Pin-App "Microsoft Edge" -unpin
+Pin-App "Photos" -unpin
