4c3106bda501dfffe778988e7afb56edf68f9753
Chris Franco <11533365+Francommit@users.noreply.github.com>
2019-05-09 11:48:06 +1000

    Update bootstrap_docker_box.ps1

--- bootstrap_docker_box.ps1
+++ bootstrap_docker_box.ps1
@@ -1,6 +1,10 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install git.install -y
+choco install openssh -y
 Install-Module DockerMsftProvider -Force
 Install-Package Docker -ProviderName DockerMsftProvider -Force
-Install-WindowsFeature Containers -Force
+Install-WindowsFeature Containers
 Restart-Computer
+
+# ssh-keygen -t rsa -C "whatever"
+# Get-Content C:\Users\Administrator\.ssh\id_rsa
