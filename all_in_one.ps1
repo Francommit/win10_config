6720d18258a3f521151c6dc7688a4698975314d2
Chris Franco <11533365+Francommit@users.noreply.github.com>
2019-08-28 11:42:40 +1000

    Added all in one script

---
+++ all_in_one.ps1
@@ -0,0 +1,132 @@
+# Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+# iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Scoop
+iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+
+Write-Host "Setting the local timezone."
+Invoke-Expression 'tzutil /s "AUS Eastern Standard Time"'
+
+Write-Host "Setting Power Settings: Disable Hibernation, Disable Standby, Set Various Timeouts"
+powercfg /h off
+powercfg -change -hibernate-timeout-dc 0
+powercfg -change -hibernate-timeout-ac 0
+powercfg -change -standby-timeout-dc 0
+powercfg -change -standby-timeout-ac 0
+powercfg -change -monitor-timeout-dc 0
+powercfg -change -monitor-timeout-ac 30
+powercfg -change -disk-timeout-dc 180
+powercfg -change -disk-timeout-ac 180
+
+Write-Host "Setting Windows Explorer settings: Show Hidden Files, Show File Extension, Open Explorer default to This PC"
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoRestartShell -Value 0
+$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty $key Hidden 1
+Set-ItemProperty $key HideFileExt 0
+Set-ItemProperty $key LaunchTo 1
+Set-ItemProperty $key ShowTaskViewButton 0
+
+# Show "This PC" on desktop
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -Value 0
+
+# Hide Search button / box
+Write-Host "Hiding Search Box / Button..."
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Type DWord -Value 0
+ 
+# Show all tray icons
+Write-Host "Showing all tray icons..."
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer" -Name "EnableAutoTray" -Type DWord -Value 0
+
+# Remove "People Icon" from the taskbar
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People" /V PeopleBand /T REG_DWORD /D 0 /F
+
+# Turn Off App Suggestions on Start Menu
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" /v SubscribedContent-338388Enabled /t REG_DWORD /d 0 /f >nul 2>&1
+
+# Enable Windows Containers
+Enable-WindowsOptionalFeature -Online -FeatureName containers –All
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V –All
+
+# Enable Windows Sandbox
+Enable-WindowsOptionalFeature -FeatureName "Containers-DisposableClientVM" -All -Online
+
+# WSL2
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+
+# Install Fundamentals
+choco install chocolateygui -y
+choco install jdk8 -y 
+choco install 7zip -y 
+choco install googlechrome -y 
+choco install everything --params "/service" -y
+choco install deluge -y
+choco install autohotkey -y
+choco install royalts-v5 -y
+choco install openssh -y
+choco install powershell-core -y
+scoop install ffsend
+
+# Install Source Management apps
+choco install git -y
+choco install kdiff3 -y
+choco install gitextensions -y
+
+# Install IDE
+choco install sublimetext3 -y
+choco install sublimetext3.powershellalias -y
+
+# Allowing using 'subl' on the command line
+Write-Host "Adding Sublime Text 3 to the system path as 'subl'"
+$systemPath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name Path).Path
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value ($systemPath += ';C:\Program Files\Sublime Text 3\') 
+
+# Autohotkey script, capslock to Windows, ctrl + alt + v for pasting in remote sessions
+Write-Host "Place default AHK script in Startup folder. Shell:startup to navigate there"
+
+New-Item "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\scripts.ahk" -type file -value "^!v::
+SendRaw, %Clipboard%
+return
+; Rebind Capslock to the windows key
+CapsLock::LWin
+"
+# Removing the Default Windows 10 Apps 
+Import-Module -DisableNameChecking $PSScriptRoot\..\lib\take-own.psm1
+Import-Module -DisableNameChecking $PSScriptRoot\..\lib\force-mkdir.psm1
+
+# Taken from - https://gist.github.com/tkrotoff/830231489af5c5818b15
+Get-AppxPackage Microsoft.Windows.ParentalControls | Remove-AppxPackage
+Get-AppxPackage Windows.ContactSupport | Remove-AppxPackage
+Get-AppxPackage Microsoft.Xbox* | Remove-AppxPackage
+Get-AppxPackage microsoft.windowscommunicationsapps | Remove-AppxPackage # Mail and Calendar
+Get-AppxPackage Microsoft.WindowsCamera | Remove-AppxPackage
+Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
+Get-AppxPackage Microsoft.Zune* | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsPhone | Remove-AppxPackage # Phone Companion
+Get-AppxPackage Microsoft.WindowsMaps | Remove-AppxPackage
+Get-AppxPackage Microsoft.Office.Sway | Remove-AppxPackage
+Get-AppxPackage Microsoft.Appconnector | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsFeedback* | Remove-AppxPackage
+Get-AppxPackage Microsoft.Windows.FeatureOnDemand.InsiderHub | Remove-AppxPackage
+Get-AppxPackage Microsoft.Windows.Cortana | Remove-AppxPackage
+Get-AppxPackage Microsoft.People | Remove-AppxPackage
+Get-AppxPackage Microsoft.Bing* | Remove-AppxPackage # Money, Sports, News, Finance and Weather
+Get-AppxPackage Microsoft.Getstarted | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
+Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsSoundRecorder | Remove-AppxPackage
+Get-AppxPackage Microsoft.3DBuilder | Remove-AppxPackage
+Get-AppxPackage Microsoft.Advertising.Xaml | Remove-AppxPackage
+Get-AppxPackage Microsoft.Windows.ParentalControls | Remove-AppxPackage
+Get-AppxPackage Microsoft.Windows.ContentDeliveryManager | Remove-AppxPackage
+Get-AppxPackage *Twitter* | Remove-AppxPackage
+Get-AppxPackage king.com.CandyCrushSodaSaga | Remove-AppxPackage
+Get-AppxPackage flaregamesGmbH.RoyalRevolt2 | Remove-AppxPackage
+Get-AppxPackage *Netflix | Remove-AppxPackage
+Get-AppxPackage Facebook.Facebook | Remove-AppxPackage
+Get-AppxPackage Microsoft.MinecraftUWP | Remove-AppxPackage
+Get-AppxPackage *MarchofEmpires | Remove-AppxPackage
+
+# Prevents "Suggested Applications" returning
+force-mkdir "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Cloud Content"
+Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Cloud Content" "DisableWindowsConsumerFeatures" 1
