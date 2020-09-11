18944efa674ab40c09192cbdd182f44a913ffae0
Chris Franco <11533365+Francommit@users.noreply.github.com>
2020-09-11 22:05:34 +1000

    Update all_in_one.ps1

--- all_in_one.ps1
+++ all_in_one.ps1
@@ -108,8 +108,30 @@ choco install steam epicgameslauncher -y
 # Autohotkey script, capslock to Windows, ctrl + alt + v for pasting in remote sessions
 Write-Host "Place default AHK script in Startup folder. Shell:startup to navigate there"
 
-New-Item "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\scripts.ahk" -type file -value "^!v::
+New-Item "$env:appdata\Microsoft\Windows\Start Menu\Programs\Startup\scripts.ahk" -type file -value "
+; Ctrl + alt + v --> Type out the clipboard
+^!v::
 SendRaw, %Clipboard%
+return
+
+; Shift + Alt + Left --> becomes Shift + Home
+!+Left::
+Send, {Shift down}{Home}{Shift up}
+return
+
+; Shift + Alt + Right --> becomes Shift + End
+!+Right::
+Send, {Shift down}{End}{Shift up}
+return
+
+; Google Search selected text --> Ctrl + Shift + C
+ ^+c::
+ {
+  Send, ^c
+  Sleep 50
+  Run, http://www.google.com/search?q=%clipboard%
+  Return
+ }
 return
 "
 
